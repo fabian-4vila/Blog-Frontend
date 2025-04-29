4b0439b97e205bea23dc7fdd6dc4961f8460e899
Fabian <fabianavb22@gmail.com>
2025-04-28 22:03:10 -0500

    init

--- src/app/products/features/product-list/product-list.component.ts
+++ src/app/products/features/product-list/product-list.component.ts
@@ -1,12 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { ProductsStateService } from '../../data-access/products-state.service';
+import { ProductCardComponent } from '../../ui/product-card/product-card.component';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './product-list.component.html',
-  styles: ``,
   providers: [ProductsStateService]
 })
 export default class ProductListComponent {
